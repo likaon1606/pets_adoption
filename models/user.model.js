e6b78f858b6b76a593e33bad52c39e406cef0e9d
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-06-26 11:54:40 -0600

    controllers

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import userSchema from "../schemas/users.schema";
+import userSchema from "../schemas/user.schema";
 
 export default class UserModel {
   constructor() {
