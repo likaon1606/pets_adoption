1f437456190130c107ecb7aab2075aebc722d860
likaon1606 <ariel.fuentesgarcia@gmail.com>
2025-06-28 22:57:50 -0600

    user controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -7,4 +7,62 @@ export const createUser = async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: "Error creating user", error: error.message });
   }
-}
+};
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users", error: error.message });
+  }
+};
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error: error.message });
+  }
+};
+
+export const getUserByEmail = async (req, res) => {
+  try {
+    const user = await userModel.findByEmail(req.params.email);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error: error.message });
+  }
+};
+
+
+export const updateUser = async (req, res) => {
+  try {
+    const user = await userModel.update(req.params.id, req.body);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: "Error updating user", error: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await userModel.delete(req.params.id);
+    if(!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user", error: error.message });
+  }
+};
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
-import { createUser } from "../controllers/users.controller.js";
+import { 
+  createUser, 
+  getAllUsers, 
+  getUserById, 
+  getUserByEmail ,
+  updateUser,
+  deleteUser
+} from "../controllers/users.controller.js";
 
 const router = Router();
 
 router.post("/register", createUser);
+router.get("/users", getAllUsers);
+router.get("/users/:id", getUserById);
+router.get("/users/email/:email", getUserByEmail);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
